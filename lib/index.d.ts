c7c1b188cc73e704900a0f1b8980dba89e01a4e4
devdoomari.circleci <devdoomari@gmail.com>
2018-10-19 02:20:10 +0000

    generated from:9b0e7d660342f4bd0a67f9fa26075b3f0cf5ba39 triggered by:devdoomari3 [ci skip]

---
+++ lib/index.d.ts
@@ -0,0 +1,25 @@
+/// <reference types="node" />
+import fs from 'fs-extra';
+import { m, MatchFuncType } from './matchers';
+export { m, };
+export declare type ClearRule = {
+    action: ACTIONS;
+    matcher: MatchFuncType;
+    children?: ClearRule[];
+};
+export declare enum ACTIONS {
+    DELETE = 0,
+    KEEP = 1
+}
+export declare function KEEP(matcher: MatchFuncType, children?: ClearRule[]): ClearRule;
+export declare function DEL(matcher: MatchFuncType, children?: ClearRule[]): ClearRule;
+export declare enum CLEAR_RESULT {
+    NOT_EMPTY = 0,
+    EMPTIED = 1
+}
+export declare function clearFs(args: {
+    fullPath: string;
+    stat?: fs.Stats;
+    clearRules: ClearRule[];
+    action: ACTIONS;
+}): Promise<any>;
